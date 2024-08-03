b541a2b7429482e4b67300326b38e874a694455a
Silvio Rodrigues Correa <silviorodrigues98@hotmail.com>
2024-08-03 08:01:21 -0300

    chore: Refactor ControleFinanceiro component to use CSVDropZone

--- src/components/ControleFinanceiro/ControleFinanceiro.tsx
+++ src/components/ControleFinanceiro/ControleFinanceiro.tsx
@@ -1,8 +1,6 @@
 import './ControleFinanceiro.css';
 import { DonutChart } from '@mantine/charts';
-import { Group, Text, rem } from '@mantine/core';
-import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
-import { Dropzone, MIME_TYPES } from '@mantine/dropzone';
+import { CSVDropZone } from '../CSVDropZone/CSVDropZone';
 
 //TODO Read the csv
 //TODO Parse the data
@@ -27,42 +25,7 @@ export function ControleFinanceiro() {
           size={500}
           style={{ width: 1000, height: 500 }}
         />
-        <Dropzone
-          onDrop={(files) => console.log('accepted files', files)}
-          onReject={(files) => console.log('rejected files', files)}
-          maxSize={5 * 1024 ** 2}
-          accept={[MIME_TYPES.csv]}
-        >
-          <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
-            <Dropzone.Accept>
-              <IconUpload
-                style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
-                stroke={1.5}
-              />
-            </Dropzone.Accept>
-            <Dropzone.Reject>
-              <IconX
-                style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
-                stroke={1.5}
-              />
-            </Dropzone.Reject>
-            <Dropzone.Idle>
-              <IconPhoto
-                style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
-                stroke={1.5}
-              />
-            </Dropzone.Idle>
-
-            <div>
-              <Text size="xl" inline>
-                Drag images here or click to select files
-              </Text>
-              <Text size="sm" c="dimmed" inline mt={7}>
-                Attach as many files as you like, each file should not exceed 5mb
-              </Text>
-            </div>
-          </Group>
-        </Dropzone>
+        <CSVDropZone />
         <h2>After</h2>
       </div>
     </>
