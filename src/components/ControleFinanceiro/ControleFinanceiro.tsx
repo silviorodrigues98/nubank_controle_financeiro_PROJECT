bf08c74c4432a600b00cdf71afe3b1555eb1d69e
Silvio Rodrigues Correa <silviorodrigues98@hotmail.com>
2024-08-02 22:55:57 -0300

    chore: Remove unused code and add ControleFinanceiro component

---
+++ src/components/ControleFinanceiro/ControleFinanceiro.tsx
@@ -0,0 +1,12 @@
+import "./ControleFinanceiro.css";
+
+//TODO Read the csv
+//TODO Parse the data
+//TODO Display the data in a table and in a graph
+export function ControleFinanceiro() {
+return (
+    <div className="center">
+        <h1>Controle Financeiro</h1>
+    </div>
+);
+}
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,10 @@
-import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-
+import { ControleFinanceiro } from '@/components/ControleFinanceiro/ControleFinanceiro';
 export function HomePage() {
   return (
     <>
-      <Welcome />
       <ColorSchemeToggle />
+      <ControleFinanceiro/>
     </>
   );
 }
